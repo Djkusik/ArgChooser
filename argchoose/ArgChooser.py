4cff877c63c4b60c8131f33f4e880b557e40bebe
Paweł Kusiński <KusinskiP@protonmail.com>
2020-07-11 17:00:40 +0200

    Fix lack of arg validation, fix typing, add changable default category name

--- argchoose/ArgChooser.py
+++ argchoose/ArgChooser.py
@@ -1,14 +1,18 @@
+import os
+
 from PyInquirer import style_from_dict, Token, prompt, Separator
 from argparse import ArgumentParser, Namespace
-from typing import Callable
+from typing import Callable, Dict, List
 
 
 class ArgChooser:
-    reserved: list = ['-A', '--all']
-    category_style: str = '== {} =='
+
+    reserved: List = ['-A', '--all']
+    category_print_style: str = '== {} =='
+    default_category_name: str = 'Other'
     validation_error: str = 'You must choose at least one module.'
     validation_func: Callable
-    style: dict = {
+    style: Dict = {
         Token.Separator: '#cc5454',
         Token.QuestionMark: '#673ab7 bold',
         Token.Selected: '#cc5454',
@@ -21,17 +25,20 @@ class ArgChooser:
 
     def __init__(self,
                  category_style: str = None,
-                 style: dict = None,
+                 default_category_name: str = None,
+                 style: Dict = None,
                  validation_func: Callable = None,
                  validation_error: str = None) -> None:
 
         self.parser: ArgumentParser = ArgumentParser()
-        self.args: dict = {}
-        self.parsed_args: dict = {}
-        self.categories: dict = {}
+        self.args: Dict = {}
+        self.parsed_args: Dict = {}
+        self.categories: Dict = {}
 
         if category_style is not None:
             self.change_category_formatting(category_style)
+        if default_category_name is not None:
+            self.change_default_category_name(default_category_name)
         if style is not None:
             self.change_style(style)
         if validation_func is not None:
@@ -39,14 +46,14 @@ class ArgChooser:
         else:
             self.validation_func = getattr(self, 'validate_answer')
         if validation_error is not None:
-            self.validation_error = validation_error
+            self.change_validation_error(validation_error)
 
         self.create_default_categories()
         self.add_run_all()
 
     def create_default_categories(self) -> None:
         self.categories['All'] = [Separator(self.format_category_style('All'))]
-        self.categories['None'] = [Separator(self.format_category_style('None'))]
+        self.categories[self.default_category_name] = [Separator(self.format_category_style(self.default_category_name))]
 
     def add_run_all(self) -> None:
         self.parser.add_argument('-A', '--all', help='execute all methods', action='store_true')
@@ -56,11 +63,17 @@ class ArgChooser:
             'All'
         )
 
-    def change_category_formatting(self, category_style: str):
-        self.category_style = category_style
+    def change_category_formatting(self, category_style: str) -> None:
+        self.category_print_style = category_style
 
-    def change_style(self, style: dict):
+    def change_style(self, style: Dict) -> None:
         self.style.update(style)
+
+    def change_validation_error(self, validation_error: str) -> None:
+        self.validation_error = validation_error
+
+    def change_default_category_name(self, default_category_name: str) -> None:
+        self.default_category_name = default_category_name
 
     def add_argument(self, *flags: str, help: str = None, category: str = None, method: Callable) -> None:
         if flags[0] not in self.reserved:
@@ -73,7 +86,7 @@ class ArgChooser:
         else:
             raise ValueError("Value -A and -all is reserved for 'run_all' function.")
 
-    def create_new_element(self, flag: str, help: str):
+    def create_new_element(self, flag: str, help: str) -> Dict:
         if help is not None:
             return {
                 'name': flag + ' - ' + help
@@ -83,32 +96,32 @@ class ArgChooser:
                 'name': flag
             }
 
-    def add_to_category(self, new_element: dict, category: str) -> None:
+    def add_to_category(self, new_element: Dict, category: str) -> None:
         if category is not None:
             self.add_to_named_category(new_element, category)
         else:
             self.add_to_unnamed_category(new_element)
 
-    def add_to_named_category(self, new_element: dict, category: str) -> None:
+    def add_to_named_category(self, new_element: Dict, category: str) -> None:
         if category not in self.categories:
             self.add_to_new_category(new_element, category)
         else:
             self.add_to_existing_category(new_element, category)
 
-    def add_to_unnamed_category(self, new_element: dict) -> None:
-        self.categories['None'].append(new_element)
+    def add_to_unnamed_category(self, new_element: Dict) -> None:
+        self.categories[self.default_category_name].append(new_element)
 
-    def add_to_new_category(self, new_element: dict, category: str) -> None:
+    def add_to_new_category(self, new_element: Dict, category: str) -> None:
         self.categories[category] = [
             Separator(self.format_category_style(category)),
             new_element
         ]
 
-    def add_to_existing_category(self, new_element: dict, category: str) -> None:
+    def add_to_existing_category(self, new_element: Dict, category: str) -> None:
         self.categories[category].append(new_element)
 
     def format_category_style(self, category: str) -> str:
-        return self.category_style.format(category)
+        return self.category_print_style.format(category)
 
     def execute(self) -> None:
         self.parse_args()
@@ -132,29 +145,35 @@ class ArgChooser:
             func()
 
     def run_chosen(self) -> None:
-        for func in self.parsed_args.values():
-            func()
+        if 'all' in self.parsed_args.keys() or self.validation_func(list(self.parsed_args.keys())):
+            for func in self.parsed_args.values():
+                func()
+        else:
+            print(self.validation_error)
 
     def run_menu(self) -> None:
-        choices: list = self.create_choices()
-        questions: list = self.create_questions(choices)
+        choices: List = self.create_choices()
+        questions: List = self.create_questions(choices)
         while True:
             answers = prompt(questions, style=style_from_dict(self.style))
-            # if self.validate_answer(answers['Modules']):
-            if self.validation_func(answers['Modules']):
+            if 'all' in answers['Modules'] or self.validation_func(answers['Modules']):
                 break
             else:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 print(self.validation_error)
         self.parse_menu_args(answers['Modules'])
         self.decide_action()
 
-    def create_choices(self) -> list:
+    def create_choices(self) -> List:
         choices = []
+        # Move uncategorized section to the end
+        self.categories[self.default_category_name] = self.categories.pop(self.default_category_name)
+
         for values in self.categories.values():
             choices.extend(values)
         return choices
 
-    def create_questions(self, choices: list) -> list:
+    def create_questions(self, choices: List) -> List:
         return [{
             'type': 'checkbox',
             'message': 'Select modules',
@@ -164,9 +183,9 @@ class ArgChooser:
             # 'validate': lambda answer: 'You must choose at least one module.' if len(answer) == 0 else True
         }]
 
-    # TODO use built-in validation in PyInquirer after merging fix from #PR117
-    def validate_answer(self, answers: list) -> bool:
+    # TODO use built-in validation in PyInquirer after merging fix from #PR117 and releasing new version
+    def validate_answer(self, answers: List) -> bool:
         return True if len(answers) > 0 else False
 
-    def parse_menu_args(self, chosen_args: list) -> None:
+    def parse_menu_args(self, chosen_args: List) -> None:
         self.parsed_args = dict(filter(lambda key: key[0] in chosen_args, self.args.items()))
